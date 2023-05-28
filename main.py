82b176735022e58a62118f29f5a76a954f45114e
D-Morais <moraisdionata@gmail.com>
2023-05-28 20:31:52 -0300

    Adicionando arquivos

--- main.py
+++ main.py
@@ -66,9 +66,8 @@ def main():
 
     st.set_option('deprecation.showPyplotGlobalUse', False)
     hide_st_style = """
-        <style> #MainMenu {visibility: hidden;}
+        <style> MainMenu {visibility: hidden;}
                 footer {visibility: hidden;}
-                header {visibility: hidden;}
         </style>
     """
     st.markdown(hide_st_style, unsafe_allow_html=True)
